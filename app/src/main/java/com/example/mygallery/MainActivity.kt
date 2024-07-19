799cdf6daecadd907adafec38886fc0cd81b93fe
rina <rinaanni11@gmail.com>
2024-07-19 13:39:34 +0900

    Permission

--- app/src/main/java/com/example/mygallery/MainActivity.kt
+++ app/src/main/java/com/example/mygallery/MainActivity.kt
@@ -1,16 +1,46 @@
 package com.example.mygallery
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_READ_EXTERNAL_STORAGE = 1000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        getAllPhotos()
-    }
+        // 권한이 부여되었는지 확인
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+
+                    // 권한이 허용되지 않음
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                            // 이전에 거부한 적이 있으면 설명(경고)
+                            var dlg = AlertDialog.Builder(this)
+                            dlg.setTitle("권한이 필요한 이유")
+                            dlg.setMessage("사진 정보를 얻기 위해서는 외부 저장소 권한이 필수로 필요합니다.")
+                            dlg.setPositiveButton("확인"){dialog, which-> ActivityCompat.requestPermissions(this@MainActivity,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_READ_EXTERNAL_STORAGE)
+                            }
+                            dlg.setNegativeButton("취소", null)
+                            dlg.show()
+                        } else {
+                            //처음 권한 요청
+                            ActivityCompat.requestPermissions(this@MainActivity,
+                                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_READ_EXTERNAL_STORAGE)
+                        } else {
+            //권한이 이미 제대로 허용됨
+            getAllPhotos()
+            }
+        }
 
     private fun getAllPhotos() {
         //모든 사진 정보 가져오기
