2767ca7c14037a818667f428274f524471796d3b
rina <rinaanni11@gmail.com>
2024-07-19 13:20:00 +0900

    Initial commit

--- app/src/main/java/com/example/mygallery/MainActivity.kt
+++ app/src/main/java/com/example/mygallery/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.mygallery
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getAllPhotos()
+    }
+
+    private fun getAllPhotos() {
+        //모든 사진 정보 가져오기
+        val cursor = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+        null, // 가져올 항목 배열
+        null, // 조건
+        null, // 조건
+        MediaStore.Images.ImageColumns.DATE_TAKEN + "DESC")
+
     }
 }
